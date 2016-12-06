4d974706a72af9e4bc01aaa48cb7e9a3db2f5eee
xiaoyu.123 <2640171401@qq.com>
2016-12-06 21:27:40 +0800

    ff

--- NMGLY/src/com/cont/nmgly/activity/BaseActivity.java
+++ NMGLY/src/com/cont/nmgly/activity/BaseActivity.java
@@ -5,6 +5,12 @@ import android.support.v4.app.FragmentActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
 
+
+
+
+
+
+
 public abstract class BaseActivity extends FragmentActivity implements OnClickListener{
 
 	@Override
